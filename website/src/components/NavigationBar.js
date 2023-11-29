526ebcbe1f4df07f2e9690c86a416f04f39f6917
Pharrell Kaim <79710229+PharrellKaim@users.noreply.github.com>
2023-11-29 14:43:31 +0100

    project init

---
+++ website/src/components/NavigationBar.js
@@ -0,0 +1,17 @@
+//React
+import React from "react";
+
+//Flowbite react
+import { Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from 'flowbite-react';
+
+///////////////////////////////////
+export default function NavigationBar() {
+    return(
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+            </ul>
+        </div>
+    );
+}
