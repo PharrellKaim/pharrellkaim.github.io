09272e8b6088b7e58a11fd1003de322d7839fc6b
PharrellKaim <79710229+PharrellKaim@users.noreply.github.com>
2026-05-01 11:47:45 +0200

    change slug

--- website/src/components/BlogData.js
+++ website/src/components/BlogData.js
@@ -1,10 +1,10 @@
 const posts = [
   {
-    slug: "firstpost",
-    path: "/blog/firstPost.md",
+    slug: "DeveloperBlog",
+    path: "/blog/DeveloperBlog.md",
     title: "Building my First Developer Blog",
     date: "2026-05-01",
-    slug: "mein-first-post",
+    slug: "Building-developer-Blog",
   },
 ];
 
